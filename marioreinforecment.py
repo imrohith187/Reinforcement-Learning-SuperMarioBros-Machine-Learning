bf9055b52ec3aae7c883cb15c0e7c89ca41fceaf
Rohith V <104898769+imrohith187@users.noreply.github.com>
2024-01-11 10:52:53 +0530

    Update marioreinforecment.py

--- marioreinforecment.py
+++ marioreinforecment.py
@@ -1,32 +1,38 @@
-#import the gamepi
-import gym_super_mario_bros
-#impor the joypad wrapper
-from nes_py.wrappers import JoypadSpace
-#import the SIMPLIFIED controls
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-
-
-#Setup game
-env=gym_super_mario_bros.make('SuperMarioBros-v0')
-# env=JoypadSpace(env,SIMPLE_MOVEMENT)
-# print(SIMPLE_MOVEMENT)
-# print(type(env.action_space.sample()))
-
-# Create a flag - restart or not
-done = True
-# Loop through each frame in the game
-for step in range(5000): 
-    # Start the game to begin with 
-    if done: 
-        # Start the gamee
-        env.reset()
-    # Do random actions
-    state, reward, done, info = env.step(env.action_space.sample())
-    # Show the game on the screen
-    env.render()
-# Close the game
-env.close()
-
-
-
-
+<?xml version="1.0" encoding="UTF-8"?>
+<mxfile host="app.diagrams.net">
+   <diagram name="Page-1" label="Page-1" layer="0" isLayers="1" isPage="1" pageScale="1" page="8.5" background="#ffffff">
+      <mxGraphModel dx="1230" dy="593" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" pageScale="1" page="8.5" math="0" shadow="0">
+         <root>
+            <mxCell id="0" />
+            <mxCell id="1" parent="0" />
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-4" value="" style="group" vertex="1" connectable="0" connectEdges="0" collapsible="1" collapsed="0" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-4">
+               <mxGeometry x="90" y="90" width="550" height="420" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-5" value="" style="group" vertex="1" connectable="0" connectEdges="0" collapsible="1" collapsed="0" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-5">
+               <mxGeometry x="320" y="300" width="280" height="240" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-6" style="group" vertex="1" connectable="0" connectEdges="0" collapsible="1" collapsed="0" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-6">
+               <mxGeometry x="20" y="240" width="280" height="200" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-7" style="group" vertex="1" connectable="0" connectEdges="0" collapsible="1" collapsed="0" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-7">
+               <mxGeometry x="20" y="30" width="280" height="200" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-8" style="group" vertex="1" connectable="0" connectEdges="0" collapsible="1" collapsed="0" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-8">
+               <mxGeometry x="320" y="30" width="280" height="200" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-9" value="ITradeDataSystem" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" connectable="0" connectEdges="0" collapsible="0" parent="RvdZMUa6wWXHgpmKMkUg-4" styleVal="shape=mxgraph.uml.interface;whiteSpace=wrap;html=1;" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-9">
+               <mxGeometry width="80" height="40" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-10" value="IReferenceDataSystem" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" connectable="0" connectEdges="0" collapsible="0" parent="RvdZMUa6wWXHgpmKMkUg-4" styleVal="shape=mxgraph.uml.interface;whiteSpace=wrap;html=1;" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-10">
+               <mxGeometry width="80" height="40" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-11" value="IRiskCalculator" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" connectable="0" connectEdges="0" collapsible="0" parent="RvdZMUa6wWXHgpmKMkUg-4" styleVal="shape=mxgraph.uml.interface;whiteSpace=wrap;html=1;" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-11">
+               <mxGeometry width="80" height="40" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-12" value="IReportGenerator" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" connectable="0" connectEdges="0" collapsible="0" parent="RvdZMUa6wWXHgpmKMkUg-4" styleVal="shape=mxgraph.uml.interface;whiteSpace=wrap;html=1;" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-12">
+               <mxGeometry width="80" height="40" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-13" value="TradeDataSystem" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" connectable="0" connectEdges="0" collapsible="0" parent="RvdZMUa6wWXHgpmKMkUg-6" styleVal="shape=mxgraph.uml.class;whiteSpace=wrap;html=1;" cy:nodeId="RvdZMUa6wWXHgpmKMkUg-13">
+               <mxGeometry width="120" height="60" as="geometry" />
+            </mxCell>
+            <mxCell id="RvdZMUa6wWXHgpmKMkUg-14" value="ReferenceDataSystem" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" connectable="0" connectEdges="0" collapsible="0" parent="RvdZMUa6wWXHgpmKMkUg
